ba993aa0eb56d9124c67099a2cb0338e3c29440e
Ash <ashley.phillips@fatjoe.com>
2023-11-22 13:55:05 +0000

    Add variables for styles and the app's name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+const data = {
+    app_name: "Ash's react practice",
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor: "lightblue", color: "darkslategray", padding: 10}}>
+        <h1>{data.app_name}</h1>
+        <MyButton />
+        <Paragraph />
     </div>
   );
+}
+
+function MyButton() {
+    return (
+        <button>I'm a button</button>
+    );
+}
+
+function Paragraph() {
+    return (
+        <p>Hello world</p>
+    )
 }
 
 export default App;
